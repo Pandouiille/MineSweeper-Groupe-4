b37e79619a7bedb391e916edefb022d4a3581e5d
Pandouiille <92433601+Pandouiille@users.noreply.github.com>
2023-02-28 17:02:16 +0100

    Timer Done

---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour
+{
+    public float timeValue = 120;
+    public Text timerText;
+
+    void Update()
+    {
+        if (timeValue > 0)
+        {
+            timeValue -= Time.deltaTime;
+        }
+        else
+        {
+            timeValue += 120;
+        }
+        DisplayTime(timeValue);
+    }
+
+    void DisplayTime(float TimeToDisplay)
+    {
+        if (TimeToDisplay < 0)
+        {
+            TimeToDisplay = 0;
+        }
+
+        float minutes = Mathf.FloorToInt(TimeToDisplay / 60);
+        float seconds = Mathf.FloorToInt(TimeToDisplay % 60);
+
+        timerText.text = string.Format("{00:00}:{1:00}",minutes,seconds);
+    }
+}
